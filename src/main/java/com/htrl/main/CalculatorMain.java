e57b15304d8af525c764d52d67346ee9cf019965
Haroun <haroun.tmimi@hotmail.com>
2016-02-09 19:17:28 +0100

    premier commit

--- src/main/java/com/htrl/main/CalculatorMain.java
+++ src/main/java/com/htrl/main/CalculatorMain.java
@@ -5,6 +5,7 @@ public class CalculatorMain {
 	public static void main(String[] args) {
 		System.out.println("Hello Haroun :)");
 		System.out.println("Bien git non?");
+		System.out.println("Super !");
 	}
 
 }
