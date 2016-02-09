e4c0fb41583e3f084283b7ce933da9e276edac17
Raphaël Laporte <rl@pcsol.be>
2016-02-09 20:19:32 +0100

    merci Haroun, voilà comment l'afficher

--- src/main/java/com/htrl/main/CalculatorMain.java
+++ src/main/java/com/htrl/main/CalculatorMain.java
@@ -1,11 +1,14 @@
 package com.htrl.main;
 
+import javax.swing.WindowConstants;
+
 public class CalculatorMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Haroun :)");
-		System.out.println("Bien git non?");
-		System.out.println("Super !");
+		JFrame frame=new JFrame();
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
