9c9094660958603161211de1c77f090c0e9a356d
Haroun <haroun.tmimi@hotmail.com>
2016-02-09 20:40:02 +0100

    WIP : Listener btn

--- src/main/java/com/htrl/main/JFrame.java
+++ src/main/java/com/htrl/main/JFrame.java
@@ -54,77 +54,78 @@ public class JFrame extends javax.swing.JFrame {
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(new GridLayout(4, 4, 0, 0));
 		
+		ActionListener btnListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = ((JButton)e.getSource()).getText();
+				String old = textField.getText();
+				textField.setText(old+name);
+			}
+		};
+		
 		JButton btn7 = new JButton("7");
+		btn7.addActionListener(btnListener);
 		panel.add(btn7);
 		
 		JButton btn8 = new JButton("8");
+		btn8.addActionListener(btnListener);
 		panel.add(btn8);
 		
-		JButton btnNewButton_7 = new JButton("9");
-		btnNewButton_7.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		panel.add(btnNewButton_7);
+		JButton btn9 = new JButton("9");
+		btn9.addActionListener(btnListener);
+		panel.add(btn9);
 		
-		JButton btnNewButton_8 = new JButton("+");
-		panel.add(btnNewButton_8);
+		JButton btnPlus = new JButton("+");
+		btnPlus.addActionListener(btnListener);
+		panel.add(btnPlus);
 		
 		JButton btn4 = new JButton("4");
+		btn4.addActionListener(btnListener);
 		panel.add(btn4);
 		
-		JButton btnNewButton_10 = new JButton("5");
-		btnNewButton_10.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		panel.add(btnNewButton_10);
+		JButton btn5 = new JButton("5");
+		btn5.addActionListener(btnListener);
+		panel.add(btn5);
 		
-		JButton btnNewButton_3 = new JButton("6");
-		btnNewButton_3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		panel.add(btnNewButton_3);
+		JButton btn6 = new JButton("6");
+		btn6.addActionListener(btnListener);
+		panel.add(btn6);
 		
-		JButton btnNewButton_6 = new JButton("-");
-		btnNewButton_6.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		panel.add(btnNewButton_6);
+		JButton btnMinus = new JButton("-");
+		btnMinus.addActionListener(btnListener);
+		panel.add(btnMinus);
 		
 		JButton btn1 = new JButton("1");
+		btn1.addActionListener(btnListener);
 		panel.add(btn1);
 		
-		JButton btnNewButton_9 = new JButton("2");
-		panel.add(btnNewButton_9);
+		JButton btn2 = new JButton("2");
+		btn2.addActionListener(btnListener);
+		panel.add(btn2);
 		
-		JButton btnNewButton = new JButton("3");
-		panel.add(btnNewButton);
+		JButton btn3 = new JButton("3");
+		btn3.addActionListener(btnListener);
+		panel.add(btn3);
 		
-		JButton btnNewButton_4 = new JButton("/");
-		btnNewButton_4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		panel.add(btnNewButton_4);
+		JButton btnDiv = new JButton("/");
+		btnDiv.addActionListener(btnListener);
+		panel.add(btnDiv);
 		
 		JButton btn0 = new JButton("0");
+		btn0.addActionListener(btnListener);
 		panel.add(btn0);
 		
-		JButton btnNewButton_13 = new JButton(".");
-		panel.add(btnNewButton_13);
+		JButton btnDot = new JButton(".");
+		panel.add(btnDot);
 		
-		JButton btnNewButton_14 = new JButton("Enter");
-		panel.add(btnNewButton_14);
+		JButton btnEnter = new JButton("Enter");
+		panel.add(btnEnter);
 		
-		JButton btnNewButton_15 = new JButton("X");
-		btnNewButton_15.addActionListener(new ActionListener() {
+		JButton btnX = new JButton("X");
+		btnX.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		panel.add(btnNewButton_15);
+		panel.add(btnX);
 	}
 
 }
